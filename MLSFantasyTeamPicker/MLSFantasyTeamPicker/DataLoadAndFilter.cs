34e8274199162ad04f808d76973b4dcd58c05d31
yltlatl <aaron.gardner@yahoo.com>
2016-03-12 08:25:06 -0500

    Just to save these for the computer rebuild.

---
+++ MLSFantasyTeamPicker/MLSFantasyTeamPicker/DataLoadAndFilter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MLSFantasyTeamPicker
+{
+    class DataLoadAndFilter
+    {
+        //load a text file
+
+        //load data from a sql db
+
+        //map fields to the fields used by the program
+    }
+}
